c01e42161998c941797fbdb08c4f8813b095a7ca
myOmikron <git@omikron.dev>
2023-01-04 00:03:27 +0100

    Inlined features

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 //! actix-toolbox enhances the functionality of actix-web by providing middlewares
 //! or other components that are frequently used together with actix-web.
 //!
-//! This also includes an ORM. [rorm](https://github.com/myOmikron/rorm) is used for this
+//! This also includes an ORM. [rorm](https://github.com/rorm-orm/rorm) is used for this
 #![warn(missing_docs)]
 
 /// Provides logging functionality e.g. sets up a configured logger
